2c33f27d1739871cfb201cab2cc6a549b0e081c9
taoshen182 <1227602328@qq.com>
2018-01-10 11:46:30 +0800

    spring aop 少了一个注解啊，坑啊，之前注解加错地方了

--- src/main/java/com/wangtao/aop/AopService.java
+++ src/main/java/com/wangtao/aop/AopService.java
@@ -1,11 +1,13 @@
 package com.wangtao.aop;
 
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 
 /**
  * @author : wangtao
  * @date : 2018/1/8 18:12
  */
+@EnableAspectJAutoProxy //这个配置了才生效
 @Service
 public class AopService {
     @Action("我的注解")
