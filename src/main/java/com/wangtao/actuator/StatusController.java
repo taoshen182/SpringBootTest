e89b33205acc8ca2ef7acce05bc65844f87f6eb6
taoshen182 <1227602328@qq.com>
2018-01-29 15:24:59 +0800

    spring 页面调整

--- src/main/java/com/wangtao/actuator/StatusController.java
+++ src/main/java/com/wangtao/actuator/StatusController.java
@@ -1,6 +1,7 @@
 package com.wangtao.actuator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,15 @@ public class StatusController {
     @Autowired
     StatusService statusService;
 
+    /**
+     * 自定义接口访问次数
+     */
+    @Autowired
+    private CounterService counterService;
+
     @RequestMapping("/change")
     public String change(String status) {
+        counterService.increment("my.status.count");
         System.out.println("status = " + status);
         statusService.setStatus(status);
         return "OK";
