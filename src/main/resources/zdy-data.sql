3b3cb366b684261030d5f1fdc3db5b4a2485a55b
taoshen182 <1227602328@qq.com>
2018-08-05 11:02:04 +0800

    spring security 密码加密

--- src/main/resources/zdy-data.sql
+++ src/main/resources/zdy-data.sql
@@ -1,5 +1,5 @@
-insert into SYS_USER (id,username,password) values (1,"wangtao","wangtao");
-insert into SYS_USER (id,username,password) values (2,"wtao","wtao");
+insert into SYS_USER (id,username,password) values (1,"wangtao","$2a$10$fst.HQY6FNJyPP51zTOpkO3WlfIoz4NC6dDzYgXfXZcfac9jiTHm2");
+insert into SYS_USER (id,username,password) values (2,"wtao","$2a$10$Lg4rhz8w9qN.c4dNVpZUd.Ed6p0AEauPH2dySWAyVNowI1SZ4pqDG");
 
 insert into SYS_ROLE (id,name) values (1,"ROLE_ADMIN");
 insert into SYS_ROLE (id,name) values (2,"ROLE_USER");
